5bbada844ddd2c739c348856b8abf886119f9de5
Fabio Ortiz <ortizfabio@yahoo.com>
2017-01-04 23:20:19 -0500

    clean resources currying example

---
+++ src/main/scala/com/bytetrend/sandbox/scala/curring/CleanResources.scala
@@ -0,0 +1,59 @@
+package com.bytetrend.sandbox.scala.curring
+
+import java.io.{BufferedWriter, FileOutputStream, OutputStreamWriter}
+import java.nio.file.{FileSystem, Path}
+
+import org.scalatest.prop.Configuration
+
+import scala.util.{Failure, Success, Try}
+
+/**
+  * https://www.phdata.io/try-with-resources-in-scala/
+  */
+object CleanResources extends App{
+
+  def cleanly[A, B](resource: A)(cleanup: A => Unit)(doWork: A => B): Try[B] = {
+    try {
+      Success(doWork(resource))
+    } catch {
+      case e: Exception => Failure(e)
+    }
+    finally {
+      try {
+        if (resource != null) {
+          cleanup(resource)
+        }
+      } catch {
+        case e: Exception => println(e) // should be logged
+      }
+    }
+  }
+  import scala.collection.JavaConversions._
+  val tempdir = for{
+    (k,v) <- System.getProperties
+    if("java.io.tmpdir".equals(k))
+  }yield v
+
+  tempdir.toList match  {
+    case head :: tail =>
+    cleanly(new FileOutputStream(s"$head/demo"))(_.close()) { os =>
+      val bufferedWriter = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"))
+      bufferedWriter.write("some data bytes")
+      bufferedWriter.newLine()
+      bufferedWriter.flush()
+    }
+    case Nil =>
+  }
+}
+
+
+
+object EnvironmentVariables extends App {
+  import scala.collection.JavaConversions._
+  val environmentVars = System.getenv()
+  for ((k,v) <- environmentVars) println(s"key: $k, value: $v")
+
+//  val properties = System.getProperties()
+//  for ((k,v) <- properties) println(s"key: $k, value: $v")
+
+}
