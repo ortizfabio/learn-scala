7d79c19d74312cec68c9fab3a857e8701ca11184
Fabio Ortiz <ortizfabio@yahoo.com>
2017-11-14 21:59:19 -0500

    Bynary tree rotation

--- src/main/scala/com/bytetrend/sandbox/scala/challenge/BlobBoundary.scala
+++ src/main/scala/com/bytetrend/sandbox/scala/challenge/BlobBoundary.scala
@@ -2,7 +2,7 @@ package com.bytetrend.sandbox.scala.challenge
 
 import scala.collection.mutable
 
-class BlobBoundary extends App {
+object BlobBoundary extends App {
   implicit def int2boolean(b: Int) = if (b == 0) false else true
 
   val test: Array[Array[Boolean]] = Array(
@@ -19,16 +19,43 @@ class BlobBoundary extends App {
   )
 
   def search(matrix: Array[Array[Boolean]]): (Int, Int, Int, Int, Int) = {
-    var set: mutable.Set[(Int, Int)] = mutable.Set()[(Int, Int)]
+    var visited: mutable.Set[(Int, Int)] = mutable.Set()
+    var blob: mutable.Set[(Int, Int)] = mutable.Set()
+    var top = 0
+    var bottom = matrix.length - 1
+    var left = 0
+    var right = matrix(0).length - 1
 
-    def findFirst(matrix: Array[Array[Boolean]]): Option[(Int, Int)] = {
-      val r2: Iterator[(Int, Int)] =for (x <- matrix.indices.toIterator; y <- matrix(x).indices.toIterator; set += (x,y);  if matrix(x)(y) == true)
-        yield (x, y)
+    def test(x: Int, y: Int): Boolean = {
+      visited += ((x, y))
+      matrix(x)(y)
+    }
+
+    def findFirst(): Option[(Int, Int)] = {
+      val r2: Iterator[(Int, Int)] = for (x <- matrix.indices.toIterator;
+                                          y <- matrix(x).indices.toIterator;
+                                         if test(x,y) ) yield (x, y)
       if (r2.hasNext)
         Some(r2.next)
       else
         None
     }
 
+    def findBlob(prev: (Int, Int),curr: (Int, Int)): (Int, Int, Int, Int, Int) = {
+
+      (curr._1, curr._2, visited.size, blob.size, 0)
+
+    }
+
+    findFirst match {
+      case Some((x, y)) => {
+        blob += ((x, y))
+        left = y
+        findBlob((x, y - 1),(x, y))
+      }
+      case None => (0, 0, 0, 0, 0)
+    }
   }
+
+  println(search(test))
 }
